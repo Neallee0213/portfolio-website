75a148f1b083971e6b47594bbefda83b1b7b77bd
Neallee0213 <hyunjoon.lee0213@gmail.com>
2019-09-11 00:10:34 -0400

    first commit

--- src/components/NavButtom.js
+++ src/components/NavButtom.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { Container, Row, Col} from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import styled from "styled-components";
 import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faFile } from "@fortawesome/free-regular-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Resume from "../assets/docs/Hyun-joon Lee Resume.pdf"
 
 
 const Styles = styled.div`
@@ -29,15 +31,18 @@ const Styles = styled.div`
 export const NavButton = () => (
     <Styles>
         <Container>
-            <Row  className="button-row">
+            <Row className="button-row">
                 <Col className="button-col d-flex justify-content-center">
                     <a href={"https://www.linkedin.com/in/hyun-joon-lee"} target="_blank" rel="noopener noreferrer">
                         <FontAwesomeIcon icon={faLinkedin} className="12"></FontAwesomeIcon>
                     </a>
+                    <a href={Resume} target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faFile}></FontAwesomeIcon>
+                    </a>
                     <a href={"https://github.com/Neallee0213"} target="_blank" rel="noopener noreferrer">
                         <FontAwesomeIcon icon={faGithubSquare}></FontAwesomeIcon>
                     </a>
-                    </Col>
+                </Col>
             </Row>
         </Container>
     </Styles>
