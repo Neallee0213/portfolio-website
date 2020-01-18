adf50d23feb9ee7d3d56ab3ad5506859c21dda0d
Neal Lee <hyunjoon.lee0213@gmail.com>
2020-01-18 17:21:48 -0500

    footer updated

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -8,9 +8,13 @@ export class Footer extends Component {
     render() {
         return (
             <div className="container-fluid footer">
-                <div className="row">
-                    <div className="col-12 footer-col footer-icons">
-                        <div className="row justify-content-center">
+                <div className="row justify-content-center">
+                    {/* <div className="col-12 footer-col footer-icons"> */}
+                    <div className="col-12 footer_col footer_icons">
+                        <li><a className="footer_links first_links" href={"https://www.linkedin.com/in/hyun-joon-lee"} target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+                        <li><a className="footer_links" href={resume} target="_blank" rel="noopener noreferrer">Resume</a></li>
+                        <li><a className="footer_links last_links" href={"https://github.com/Neallee0213"} target="_blank" rel="noopener noreferrer">GitHub</a></li>
+                        {/* <div className="row justify-content-center">
                             <div className="col-12 col-md-3">
                                 <a href={"https://www.linkedin.com/in/hyun-joon-lee"} target="_blank" rel="noopener noreferrer"><LinkedInIcon className="linkedin" /><span>LinkedIn</span></a>
                             </div>
@@ -20,7 +24,7 @@ export class Footer extends Component {
                             <div className="col-12 col-md-3">
                                 <a href={"https://github.com/Neallee0213"} target="_blank" rel="noopener noreferrer"><GitHubIcon className="github" /><span>GitHub</span></a>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
